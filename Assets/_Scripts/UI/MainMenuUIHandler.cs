1c89356fdbc62654d980ab8d7acc47805094b004
Bennet <bennet@metricminds.com>
2024-05-31 10:46:18 +0200

    Add ui for custom game start

--- Assets/_Scripts/UI/MainMenuUIHandler.cs
+++ Assets/_Scripts/UI/MainMenuUIHandler.cs
@@ -7,34 +7,31 @@ using UnityEngine.UI;
 
 public class MainMenuUIHandler : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject prefab;
-    [SerializeField]
-    private Transform parent;
-    [SerializeField]
-    private GameObject SettingsPanel;
-    
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private Transform parent;
+    [SerializeField] private GameObject SettingsPanel;
+    [SerializeField] private GameObject CustomGamePanel;
+
     private GameManager gameManager;
     private string[] names;
-    
-    
+
+
     #region Unity Callbacks
-        
+
     private void Awake()
     {
-        
     }
 
     private void Start()
     {
         CloseSettings();
-        names = new[] { "Start Game", "Custom Game", "Level Editor", "Options", "Exit" }; 
+        names = new[] { "Start Game", "Custom Game", "Level Editor", "Options", "Exit" };
         gameManager = ServiceLocator.Instance.GetService<GameManager>();
 
         for (int i = 0; i < 5; i++)
         {
             var go = Instantiate(prefab, parent);
-            go.name = names[i] +"_Button";
+            go.name = names[i] + "_Button";
             TMP_Text textComp = go.GetComponentInChildren<TMP_Text>();
             textComp.gameObject.name = names[0] + "_Text";
             textComp.text = names[i];
@@ -42,35 +39,37 @@ public class MainMenuUIHandler : MonoBehaviour
             textComp.fontStyle = FontStyles.Bold;
             Button button = go.GetComponent<Button>();
             var i1 = i;
-            button.onClick.AddListener(()=>
-            {
-                PerformAction(names[i1]);
-            });
+            button.onClick.AddListener(() => { PerformAction(names[i1]); });
         }
     }
+
     #endregion
-    
+
     private void PerformAction(string button)
     {
+        CloseSettings();
+        CloseCustomGame();
+        
         if (button == names[0])
         {
-            CloseSettings();
             gameManager.SetGameState(GameStates.Game);
         }
+
         if (button == names[1])
         {
-            CloseSettings();
-            //TODO Add screen to select custom level and theme
+            CustomGamePanel.SetActive(!CustomGamePanel.activeSelf);
         }
+
         if (button == names[2])
         {
-            CloseSettings();
             gameManager.SetGameState(GameStates.LevelEditor);
         }
+
         if (button == names[3])
         {
             SettingsPanel.SetActive(true);
         }
+
         if (button == names[4])
         {
             Application.Quit();
@@ -82,7 +81,13 @@ public class MainMenuUIHandler : MonoBehaviour
 
     public void CloseSettings()
     {
-        SettingsPanel.SetActive(false);
+        if (SettingsPanel.activeSelf)
+            SettingsPanel.SetActive(false);
     }
 
+    public void CloseCustomGame()
+    {
+        if (CustomGamePanel.activeSelf)
+            CustomGamePanel.SetActive(false);
+    }
 }
