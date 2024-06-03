707f576537dc858f39f1c71a05f1f5732ee989d9
Bennet <bennet@metricminds.com>
2024-06-03 06:35:04 +0200

    Add back to menu in level editor and game scene settings

--- Assets/_Scripts/UI/GameSettingsUIHandler.cs
+++ Assets/_Scripts/UI/GameSettingsUIHandler.cs
@@ -3,21 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class GameSettingsUIHandler : MonoBehaviour
 {
     [SerializeField]
     private GameObject settingsPanel;
+    [SerializeField]
+    private Button menuButton;
+
+    private GameManager gameManager;
     #region Unity Callbacks
         
     private void Awake()
     {
         settingsPanel.SetActive(false);
+        menuButton.onClick.AddListener(ReturnToMenu);
     }
 
     private void Start()
     {
-        
+        gameManager = ServiceLocator.Instance.GetService<GameManager>();
     }
 
     #endregion
@@ -28,5 +34,10 @@ public class GameSettingsUIHandler : MonoBehaviour
         {
            settingsPanel.SetActive(!settingsPanel.activeSelf); 
         }
+    }
+
+    private void ReturnToMenu()
+    {
+        gameManager.SetGameState(GameStates.Menu);
     }
 }
