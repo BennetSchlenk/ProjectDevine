f5b6f2fd64132865259e9e660aab814caa42f12f
Unai <unairf@gmail.com>
2024-06-03 14:22:15 +0200

    Add angle limit to FaceTarget

--- Assets/_Scripts/Towers/FaceTarget.cs
+++ Assets/_Scripts/Towers/FaceTarget.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class FaceTarget : MonoBehaviour
 {
     [SerializeField] private bool rotateX = true;
+    [SerializeField] private Vector2 rotationLimitsX = new Vector2(-360, 360);
     [SerializeField] private bool rotateY = true;
+    [SerializeField] private Vector2 rotationLimitsY = new Vector2(-360, 360);
     [SerializeField] private bool rotateZ = true;
+    [SerializeField] private Vector2 rotationLimitsZ = new Vector2(-360, 360);
     [SerializeField] private float rotationSpeed = 5f;
 
     private Transform target;
@@ -43,11 +46,14 @@ public class FaceTarget : MonoBehaviour
         Quaternion lookRotation = Quaternion.LookRotation(direction);
 
         if (!rotateX)
-            lookRotation.x = transform.rotation.x;
+        {
+            // Aply the rotation limits
+            lookRotation.x = Mathf.Clamp(lookRotation.x, rotationLimitsX.x, rotationLimitsX.y);
+        }
         if (!rotateY)
-            lookRotation.y = transform.rotation.y;
+            lookRotation.y = Mathf.Clamp(lookRotation.y, rotationLimitsY.x, rotationLimitsY.y);
         if (!rotateZ)
-            lookRotation.z = transform.rotation.z;
+            lookRotation.z = Mathf.Clamp(lookRotation.z, rotationLimitsZ.x, rotationLimitsZ.y);
 
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
     }
